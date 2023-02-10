342947056e3b526bafa433dd6af17d79cbc8b5a6
ishanog <88334650+ishanog@users.noreply.github.com>
2023-02-10 19:45:46 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+var productsContainer = document.querySelector("#products");
+
+fetch("products.json")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(product => {
+      productsContainer.innerHTML += `
+        <div class="product">
+        <img src="${product.image}" alt="${product.name}">
+          <h3><b>${product.name}</b></h3>
+          <p><b>${product.price}</b></p>
+          <p><b>${product.description}</b></p>
+      </div>
+    `;
+    });
+  });
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function myFunction() {
+  location.replace("https://www.w3schools.com")
+}
